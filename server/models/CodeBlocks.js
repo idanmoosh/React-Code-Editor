27e97f010b24e4020bc59d29096e140b6b906fa5
Idan Moosh Haim <idanmoosh@gmail.com>
2022-12-20 14:06:51 +0200

    changed db to mongoose

--- server/models/CodeBlocks.js
+++ server/models/CodeBlocks.js
@@ -1,9 +1,17 @@
 const mongoose = require('mongoose');
 
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://localhost:27017/test');
+}
+
 const CodeBlocks = new mongoose.Schema({
   _id: String,
-  data: Object,
-  answer: Object,
+  title: String,
+  link: String,
+  data: String,
+  answer: String,
 });
 
 module.exports = mongoose.model('CodeBlocks', CodeBlocks);
